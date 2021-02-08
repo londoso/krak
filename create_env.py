3ab55cc28e2a0554ae191ba7c203ca1da1973db0
londoso <londoso@gmail.com>
2021-02-08 18:15:24 -0500

    Despliegue con documentacion

--- create_env.py
+++ create_env.py
@@ -14,7 +14,7 @@ def create_env():
         cur.execute('drop schema if exists repo cascade;')
         cur.execute('create schema if not exists repo;')
         #cur.execute('drop table if exists repo.indicador;')
-        cur.execute('create table repo.indicador (PK SERIAL PRIMARY KEY, COUNTRYNAME varchar(80) not null, INDICATORNAME varchar(200) not null, YEAR int not null, VALUE numeric(3));')
+        cur.execute('create table repo.indicador (PK SERIAL PRIMARY KEY, COUNTRYNAME varchar(80) not null, INDICATORNAME varchar(200) not null, YEAR int, VALUE numeric(100,50));')
 
         print('Environment repo-indicador Created')
        
